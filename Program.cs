3b7d2c729a808967509a818c1127c57237f9631b
Nasim <nasim.roshan84@gmail.com>
2019-09-30 08:59:36 +0200

    Add initial menu and exit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using static System.Console;
 namespace Resturant
 {
     class Program
@@ -7,7 +7,39 @@ namespace Resturant
         static void Main(string[] args)
         {
 
+            bool shouldNotExit = true;
+
+            while (shouldNotExit)
+            {
+                WriteLine("1. Add order");
+                WriteLine("2. List orders");
+                WriteLine("3. Exit");
+
+                ConsoleKeyInfo keyPressed = ReadKey(true);
+
+                Clear();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+
+                        shouldNotExit = false;
+
+                        break;
+                }
+
+                Clear();
+            }
+
+
 
         }
     }
 }
+ 
